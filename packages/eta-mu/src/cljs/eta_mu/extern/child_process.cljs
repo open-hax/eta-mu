715f6b0c524626e748de239ac2ff2a6a49b9e873
Err <foamy125@gmail.com>
2026-07-25 17:09:24 -0500

    fix(tools): terminate shell process trees on abort

--- packages/eta-mu/src/cljs/eta_mu/extern/child_process.cljs
+++ packages/eta-mu/src/cljs/eta_mu/extern/child_process.cljs
@@ -98,36 +98,92 @@
         (.on child "close" (fn [code] (resolve {:exit (or code 0) :stdout @stdout :stderr @stderr})))
         (.on child "error" (fn [err] (resolve {:exit 1 :stdout @stdout :stderr (.-message err)})))))))
 
+(defn- terminate-process-tree!
+  "Best-effort termination of child and descendants created by a shell command."
+  [child detached?]
+  (let [pid (.-pid child)]
+    (try
+      (if (= "win32" (.-platform js/process))
+        (.spawn cp "taskkill" #js ["/pid" (str pid) "/T" "/F"]
+                #js {"stdio" "ignore" "windowsHide" true})
+        (if (and detached? pid)
+          (js/process.kill (- pid) "SIGTERM")
+          (.kill child "SIGTERM")))
+      (catch :default _
+        (try
+          (.kill child "SIGTERM")
+          (catch :default _ nil))))))
+
 (defn exec-shell-capture
   "Execute a shell command string, capture stdout/stderr, and return a promise
-  of {:exit <code> :stdout <string> :stderr <string> :timed-out? <bool>}.
+  of {:exit <code> :stdout <string> :stderr <string> :timed-out? <bool>
+      :aborted? <bool>}.
 
-  When `timeout-ms` is a positive number, the process is killed and the exit
-  code reported as 124 if it has not closed within that window. Stdio is
-  piped; the current working directory is process.cwd()."
-  [command timeout-ms]
-  (js/Promise.
+  When `timeout-ms` is positive, the process tree is terminated and exit 124 is
+  reported if it has not closed within that window. When `signal` aborts, the
+  process tree is terminated and exit 130 is reported. Stdio is piped; the
+  current working directory is process.cwd()."
+  ([command timeout-ms]
+   (exec-shell-capture command timeout-ms nil))
+  ([command timeout-ms signal]
+   (js/Promise.
     (fn [resolve _reject]
       (let [stdout (atom "")
             stderr (atom "")
-            timed-out (atom false)
-            child (.spawn cp command #js {"shell" true "stdio" "pipe" "cwd" (js/process.cwd)})
-            timer (when (and timeout-ms (pos? timeout-ms))
-                    (js/setTimeout (fn []
-                                     (reset! timed-out true)
-                                     (.kill child))
-                                   timeout-ms))]
+            timed-out? (atom false)
+            aborted? (atom false)
+            settled? (atom false)
+            detached? (not= "win32" (.-platform js/process))
+            child (.spawn cp command #js {"shell" true
+                                          "stdio" "pipe"
+                                          "cwd" (js/process.cwd)
+                                          "detached" detached?})
+            timer* (atom nil)
+            abort-listener* (atom nil)
+            cleanup! (fn []
+                       (when-let [timer @timer*]
+                         (js/clearTimeout timer))
+                       (when (and signal @abort-listener*)
+                         (.removeEventListener signal "abort" @abort-listener*)))
+            finish! (fn [result]
+                      (when (compare-and-set! settled? false true)
+                        (cleanup!)
+                        (resolve result)))
+            abort! (fn []
+                     (reset! aborted? true)
+                     (terminate-process-tree! child detached?))]
         (.on (.-stdout child) "data" (fn [data] (swap! stdout str data)))
         (.on (.-stderr child) "data" (fn [data] (swap! stderr str data)))
-        (.on child "close" (fn [code]
-                             (when timer (js/clearTimeout timer))
-                             (resolve {:exit (if @timed-out 124 (or code 0))
-                                       :stdout @stdout :stderr @stderr
-                                       :timed-out? @timed-out})))
-        (.on child "error" (fn [err]
-                             (when timer (js/clearTimeout timer))
-                             (resolve {:exit 1 :stdout @stdout :stderr (.-message err)
-                                       :timed-out? @timed-out})))))))
+        (.on child "close"
+             (fn [code]
+               (finish! {:exit (cond
+                                 @aborted? 130
+                                 @timed-out? 124
+                                 :else (or code 0))
+                         :stdout @stdout
+                         :stderr @stderr
+                         :timed-out? @timed-out?
+                         :aborted? @aborted?})))
+        (.on child "error"
+             (fn [err]
+               (finish! {:exit (if @aborted? 130 1)
+                         :stdout @stdout
+                         :stderr (.-message err)
+                         :timed-out? @timed-out?
+                         :aborted? @aborted?})))
+        (when (and timeout-ms (pos? timeout-ms))
+          (reset! timer*
+                  (js/setTimeout
+                   (fn []
+                     (reset! timed-out? true)
+                     (terminate-process-tree! child detached?))
+                   timeout-ms)))
+        (when signal
+          (let [listener (fn [] (abort!))]
+            (reset! abort-listener* listener)
+            (.addEventListener signal "abort" listener #js {:once true})
+            (when (.-aborted signal)
+              (abort!)))))))))
 
 (defn spawn-inherit
   "Spawn a command with inherited stdio and return a promise that resolves with its exit code."
