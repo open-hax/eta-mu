37092154dfa232811b3ce96cb5468240ab923a1c
Err <foamy125@gmail.com>
2026-07-26 23:24:20 -0500

    test(axxium): execute organization constraints in Postgres

--- packages/axxium/test/cljs/axxium/db_test.cljs
+++ packages/axxium/test/cljs/axxium/db_test.cljs
@@ -3,6 +3,10 @@
             [cljs.test :refer [deftest is testing]]
             [clojure.string :as str]))
 
+(defn- integration-db?
+  []
+  (= "true" (aget (.-env js/process) "AXXIUM_TEST_DB")))
+
 (deftest schema-sql-adds-organization-scope-idempotently-test
   (testing "new and existing installs receive the organization column"
     (is (str/includes? db/schema-sql "org_id TEXT"))
@@ -34,3 +38,48 @@
   (testing "organization lookup is indexed"
     (is (str/includes? db/schema-sql
                        "idx_actors_org ON actors(org_id)"))))
+
+(deftest ^:async organization-scope-database-constraint-test
+  (if-not (integration-db?)
+    (is true "PostgreSQL integration is opt-in outside CI")
+    (let [suffix (str (random-uuid))
+          org-id (str "org_test_" suffix)
+          human-id (str "human_test_" suffix)
+          agent-entity-id (str "agent_entity_test_" suffix)
+          actor-id (str "actor_test_" suffix)
+          invalid-actor-id (str "invalid_actor_test_" suffix)]
+      (try
+        (await (db/init-schema!))
+        (await (db/query
+                "INSERT INTO entities (id, kind) VALUES ($1, 'org'), ($2, 'human'), ($3, 'agent')"
+                [org-id human-id agent-entity-id]))
+        (await (db/query
+                "INSERT INTO actors (id, entity_id, org_id) VALUES ($1, $2, $3)"
+                [actor-id agent-entity-id org-id]))
+        (is (some? (await (db/query-one
+                           "SELECT id FROM actors WHERE id = $1"
+                           [actor-id]))))
+
+        (try
+          (await (db/query
+                  "INSERT INTO actors (id, entity_id, org_id) VALUES ($1, $2, $3)"
+                  [invalid-actor-id agent-entity-id human-id]))
+          (is false "non-organization entity must not be accepted as org_id")
+          (catch :default error
+            (is (= "23514" (.-code error)))))
+
+        (try
+          (await (db/query
+                  "UPDATE entities SET kind = 'service' WHERE id = $1"
+                  [org-id]))
+          (is false "referenced organization entity kind must remain org")
+          (catch :default error
+            (is (= "23514" (.-code error)))))
+
+        (finally
+          (try
+            (await (db/query "DELETE FROM actors WHERE id = $1" [actor-id]))
+            (await (db/query "DELETE FROM entities WHERE id IN ($1, $2, $3)"
+                             [org-id human-id agent-entity-id]))
+            (catch :default _ nil))
+          (await (.end @db/pool)))))))
