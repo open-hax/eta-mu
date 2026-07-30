f55da365e5e6830dd0908321f27f81c5ceb21660
Err <foamy125@gmail.com>
2026-07-30 13:42:31 -0500

    Bound Receipt River Git subprocesses

--- packages/receipt-river/src/cljs/eta_mu/receipt_river/extern/git.cljs
+++ packages/receipt-river/src/cljs/eta_mu/receipt_river/extern/git.cljs
@@ -3,6 +3,8 @@
   (:require [clojure.string :as str]
             ["node:child_process" :as cp]))
 
+(def ^:private default-timeout-ms 30000)
+
 (defn close-result
   "Decode Node's child-process close values into a shaped Git result."
   [code signal stdout stderr]
@@ -23,21 +25,45 @@
          (= "true" (str/trim (.-stdout result))))))
 
 (defn exec-at
-  [cwd args]
-  (js/Promise.
-   (fn [resolve _reject]
-     (let [stdout (atom "")
-           stderr (atom "")
-           child (.spawn cp "git" (clj->js args)
-                         #js {:cwd cwd :stdio "pipe"})]
-       (.on (.-stdout child) "data" #(swap! stdout str %))
-       (.on (.-stderr child) "data" #(swap! stderr str %))
-       (.on child "close"
-            (fn [code signal]
-              (resolve (close-result code signal @stdout @stderr))))
-       (.on child "error"
-            (fn [error]
-              (resolve {:exit 1
-                        :signal nil
-                        :stdout ""
-                        :stderr (.-message error)})))))))
+  ([cwd args]
+   (exec-at cwd args {}))
+  ([cwd args {:keys [timeout-ms kill-signal]
+              :or {timeout-ms default-timeout-ms
+                   kill-signal "SIGKILL"}}]
+   (js/Promise.
+    (fn [resolve _reject]
+      (let [stdout (atom "")
+            stderr (atom "")
+            settled? (atom false)
+            timer (atom nil)
+            child (.spawn cp "git" (clj->js args)
+                          #js {:cwd cwd :stdio "pipe"})
+            finish! (fn [result]
+                      (when (compare-and-set! settled? false true)
+                        (when @timer
+                          (js/clearTimeout @timer))
+                        (resolve result)))]
+        (.setEncoding (.-stdout child) "utf8")
+        (.setEncoding (.-stderr child) "utf8")
+        (.on (.-stdout child) "data" #(swap! stdout str %))
+        (.on (.-stderr child) "data" #(swap! stderr str %))
+        (.on child "close"
+             (fn [code signal]
+               (finish! (close-result code signal @stdout @stderr))))
+        (.on child "error"
+             (fn [error]
+               (finish! {:exit 1
+                         :signal nil
+                         :stdout (str/trim @stdout)
+                         :stderr (.-message error)})))
+        (reset! timer
+                (js/setTimeout
+                 (fn []
+                   (.kill child kill-signal)
+                   (finish! {:exit 1
+                             :status :timeout
+                             :signal kill-signal
+                             :stdout (str/trim @stdout)
+                             :stderr (str/trim @stderr)
+                             :timeout-ms timeout-ms}))
+                 timeout-ms)))))))
