bd0ddd83cf2bc276991c90bdea71609cb09bda8e
Err <foamy125@gmail.com>
2026-08-05 16:32:48 -0500

    fix(rheos): scope duplicate preflight to current tasks

--- packages/rheos/src/rheos/backend/infra/github_issue_preflight.cljs
+++ packages/rheos/src/rheos/backend/infra/github_issue_preflight.cljs
@@ -3,29 +3,37 @@
   (:require [clojure.string :as str]
             [rheos.backend.infra.github-issues :as github]))
 
-(defn duplicate-uuid-claims [issues]
-  (->> issues
-       (reduce (fn [claims issue]
-                 (if-let [uuid (github/extract-task-uuid issue)]
-                   (update claims uuid (fnil conj []) (:number issue))
-                   claims))
-               {})
-       (keep (fn [[uuid issue-numbers]]
-               (when (> (count issue-numbers) 1)
-                 {:uuid uuid
-                  :issue-numbers (vec (sort issue-numbers))})))
-       (sort-by :uuid)
-       vec))
+(defn duplicate-uuid-claims
+  ([issues]
+   (duplicate-uuid-claims issues nil))
+  ([issues allowed-uuids]
+   (->> issues
+        (reduce (fn [claims issue]
+                  (if-let [uuid (github/extract-task-uuid issue)]
+                    (if (or (nil? allowed-uuids) (contains? allowed-uuids uuid))
+                      (update claims uuid (fnil conj []) (:number issue))
+                      claims)
+                    claims))
+                {})
+        (keep (fn [[uuid issue-numbers]]
+                (when (> (count issue-numbers) 1)
+                  {:uuid uuid
+                   :issue-numbers (vec (sort issue-numbers))})))
+        (sort-by :uuid)
+        vec)))
 
-(defn assert-no-duplicate-uuid-claims! [issues]
-  (let [duplicates (duplicate-uuid-claims issues)]
-    (when (seq duplicates)
-      (throw
-       (js/Error.
-        (str "Duplicate GitHub issue UUID markers:\n"
-             (str/join
-              "\n"
-              (map (fn [{:keys [uuid issue-numbers]}]
-                     (str "- " uuid " on issues "
-                          (str/join ", " (map #(str "#" %) issue-numbers))))
-                   duplicates))))))))
+(defn assert-no-duplicate-uuid-claims!
+  ([issues]
+   (assert-no-duplicate-uuid-claims! issues nil))
+  ([issues allowed-uuids]
+   (let [duplicates (duplicate-uuid-claims issues allowed-uuids)]
+     (when (seq duplicates)
+       (throw
+        (js/Error.
+         (str "Duplicate GitHub issue UUID markers for current Rheos tasks:\n"
+              (str/join
+               "\n"
+               (map (fn [{:keys [uuid issue-numbers]}]
+                      (str "- " uuid " on issues "
+                           (str/join ", " (map #(str "#" %) issue-numbers))))
+                    duplicates)))))))))
